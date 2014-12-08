88bc729915a11190d64771964ac7737ae66bcdf3
Kate Ross <kateross1227@gmail.com>
2014-12-08 06:17:03 -0700

    Update CheckersMove.java

--- src/CheckersMove.java
+++ src/CheckersMove.java
@@ -6,7 +6,6 @@ class CheckersMove {
    int fromRow, fromCol;  // Position of piece to be moved.
    int toRow, toCol;      // Square it is to move to.
    CheckersMove(int r1, int c1, int r2, int c2) {
-        // Constructor.  Just set the values of the instance variables.
       fromRow = r1;
       fromCol = c1;
       toRow = r2;
@@ -18,4 +17,8 @@ class CheckersMove {
         // rows.  (In a regular move, it only moves one row.)
       return (fromRow - toRow == 2 || fromRow - toRow == -2);
    }
-}  // end class CheckersMove.
+   
+   public int getFromRow() {
+     return fromRow;
+   }
+}
